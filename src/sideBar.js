d676cf32085f4fdbfaef127a5784f43c57fb737d
Aniket Datta <datta.aniket7@gmail.com>
2021-10-30 21:18:39 +0530

    icom-buttons

--- src/sideBar.js
+++ src/sideBar.js
@@ -1,12 +1,16 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { FaFire, FaPoo } from 'react-icons/fa';
+
 const sideBar = () => {
 	return (
 		<div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
-			<i>A</i>
-			<i>B</i>
-			<i>C</i>
-			<i>D</i>
-			<i>E</i>
+			<SideBarIcon icon={<FaFire size="28" />} />
+			<SideBarIcon icon={<BsPlus size="32" />} />
+			<SideBarIcon icon={<BsFillLightningFill size="20" />} />
+			<SideBarIcon icon={<FaPoo size="20" />} />
 		</div>
 	);
 };
+
+const SideBarIcon = ({ icon }) => <div className="sidebar-icon">{icon}</div>;
 export default sideBar;
